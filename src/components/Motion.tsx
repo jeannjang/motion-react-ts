db69ca3f134b10473669894eff43573961e0f585
Jean <jeanjyjh@gmail.com>
2024-12-04 10:10:11 +1300

    feat: Implement basic animation in Motion.tsx
    - transitions

---
+++ src/components/Motion.tsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import { motion } from "motion/react";
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  background: linear-gradient(135deg, #e09, #d0e);
+  border-radius: 15px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+function Motion() {
+  return (
+    <Wrapper>
+      <Box
+        transition={{ duration: 0.25, type: "spring", stiffness: 260 }}
+        initial={{ scale: 0 }}
+        animate={{ scale: 1, rotateZ: 360 }}
+      />
+    </Wrapper>
+  );
+}
+
+export default Motion;
