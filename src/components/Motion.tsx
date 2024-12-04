0e2f6b57194712689d772902f2fe4f65c7b7a6d5
Jean <jeanjyjh@gmail.com>
2024-12-05 10:20:55 +1300

    feat: give animations to parent and children with motion variants

--- src/components/Motion.tsx
+++ src/components/Motion.tsx
@@ -10,26 +10,66 @@ const Wrapper = styled.div`
 `;
 
 const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
+  width: 300px;
+  height: 300px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
   background: linear-gradient(135deg, #e09, #d0e);
   border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Variants = {
-  initial: { scale: 0 },
+const Circle = styled(motion.div)`
+  height: 100px;
+  width: 100px;
+  border-radius: 50%;
+  place-self: center;
+  background-color: #fff;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants = {
+  initial: {
+    opacity: 0,
+    scale: 0,
+  },
   animate: {
+    opacity: 1,
     scale: 1,
-    rotateZ: 360,
-    transition: { duration: 0.5, type: "spring", stiffness: 260 },
+    transition: {
+      type: "spring",
+      stiffness: 260,
+      duration: 0.5,
+      delayChildren: 0.3,
+      staggerChildren: 0.3,
+    },
+  },
+};
+
+const circleVariants = {
+  initial: {
+    opacity: 0,
+    y: 50,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      stiffness: 260,
+    },
   },
 };
 
 function Motion() {
   return (
     <Wrapper>
-      <Box variants={Variants} initial="initial" animate="animate" />
+      <Box variants={boxVariants} initial="initial" animate="animate">
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
     </Wrapper>
   );
 }
